839797ff94663bdf5b5ce672794b5c7de102c2a8
Arciniegas-C-L <arciniegas.cristian2341@gmail.com>
2025-05-20 11:42:28 -0500

    m

--- veterinaria/src/components/Header.jsx
+++ veterinaria/src/components/Header.jsx
@@ -1,9 +1,20 @@
 
 
 export default function Header() {
-  return (
-    <div>
-        <h1>Header</h1>
-    </div>
-  )
+    return (
+        <div className="header">
+            <div>
+                <h1>PetsFriends</h1>
+            </div>
+            <div className="menu">
+                <nav>
+                    <ul>
+                        <li>Servicios</li>
+                        <li>Nosotros</li>
+                        <li>Contactanos</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
 }
---
+++ veterinaria/src/components/JsonHome.jsx
@@ -0,0 +1,16 @@
+
+export const servicios = [
+    {
+        nombre: "Medicamentos",
+        descripcion: "Servicio de medicamentos para la salud de nuestros clientes domesticos"
+    },
+    {
+        nombre: "Productos",
+        descripcion: "Servicio de productos variados para la satisfaccion de nuestros clientes domesticos"
+    },
+    {
+        nombre: "Citas",
+        descripcion: "Servicio de citas para programar la atencion medica a nuestros clientes domesticos"
+    }
+]
+
